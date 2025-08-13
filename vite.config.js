3952f41749e349f263acd91e1a3e67c33ee588a4
Gjl <jguo0087@student.monash.edu>
2025-08-13 17:20:01 +0800

    test1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/Lab9-12/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
